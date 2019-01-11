7abdd544a48d71e8bed63c4a6150324d0ded3bed
sefadogan <sefadogan7@gmail.com>
2019-01-11 14:59:11 +0300

    Theme dressed up for every custom page :)

--- BlogApp.UI/BlogApp.UI/Controllers/ErrorController.cs
+++ BlogApp.UI/BlogApp.UI/Controllers/ErrorController.cs
@@ -8,17 +8,35 @@ namespace BlogApp.UI.Controllers
 {
     public class ErrorController : Controller
     {
-        public ActionResult NotFound()
+        [Route("sayfa-bulunamadi")]
+        public ActionResult NotFound(string aspxerrorpath)
         {
+            if (!IsNull(aspxerrorpath))
+                return RedirectToAction("NotFound");
             return View();
         }
-        public ActionResult InternalServer()
+
+        [Route("ic-sunucu-hatasi")]
+        public ActionResult InternalServer(string aspxerrorpath)
         {
+            if (!IsNull(aspxerrorpath))
+                return RedirectToAction("InternalServer");
             return View();
         }
-        public ActionResult NoAuthorization()
+
+        [Route("yetki-yok")]
+        public ActionResult NoAuthorization(string aspxerrorpath)
         {
+            if (!IsNull(aspxerrorpath))
+                return RedirectToAction("NoAuthorization");
             return View();
+        }
+
+        private bool IsNull(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return true;
+            return false;
         }
     }
 }
